88b98f7c34c37978c468250dafe1e6f249888576
Colby Williams <colby@colbycheeze.com>
2015-03-14 18:12:34 -0500

    Post validation when posting / editing

--- microscope/client/templates/posts/post_edit.js
+++ microscope/client/templates/posts/post_edit.js
@@ -1,3 +1,18 @@
+Template.postEdit.created = function() {
+  Session.set('postEditErrors', {});
+}
+
+Template.postEdit.helpers({
+  errorMessage: function (field) {
+    return Session.get('postEditErrors')[field];
+  },
+
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -11,7 +26,7 @@ Template.postEdit.events({
 
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if(error) {
-        alert(error.reason);
+        throwError(error.reason);
       } else {
         Router.go('postPage', {_id: currentPostId});
       }
