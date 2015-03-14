bf1974e832cefddfe6161f720260d2284ebd2914
Colby Williams <colby@colbycheeze.com>
2015-03-14 12:14:41 -0500

    Iron Todos app added. Meteor API with custom login. Todos can be checked done / not done.

---
+++ iron-todos/app/client/templates/layouts/master_layout/master_layout.js
@@ -0,0 +1,13 @@
+Template.MasterLayout.helpers({
+});
+
+Template.MasterLayout.events({
+  'click [data-logout]': function(e, tmpl) {
+    Meteor.logout();
+  },
+
+  'click [data-login]': function(e, tmpl) {
+    Meteor.loginWithMeteorDeveloperAccount();
+  }
+});
+
