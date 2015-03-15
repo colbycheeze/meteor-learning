7836c361e02c1f7500b847e9691246e1bf5aa6a3
Colby Williams <colby@colbycheeze.com>
2015-03-15 06:56:31 -0500

    Can view other users todos / profile, and individual todo page

--- iron-todos/app/client/templates/todos/todos_detail/todos_detail.js
+++ iron-todos/app/client/templates/todos/todos_detail/todos_detail.js
@@ -27,7 +27,13 @@ Template.TodosDetail.events({
 Template.TodosDetail.helpers({
   isMyTodo: function() {
     return this.userId === Meteor.userId();
-  }
+  },
+
+  user: function () {
+    var todo = this;
+    return Meteor.users.findOne({_id: todo.userId});
+  },
+
 });
 
 /*****************************************************************************/
