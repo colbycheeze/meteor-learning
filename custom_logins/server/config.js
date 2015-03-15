458a913601f2571836e84b1149dadf119eebf477
Colby Williams <colby@colbycheeze.com>
2015-03-15 08:32:18 -0500

    cleaned up project directories

--- custom_logins/server/config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: "github"
-});
-
-ServiceConfiguration.configurations.insert({
-  service: "github",
-  clientId: "0a802b86d5136677d81b",
-  secret: "af359bee71e568aadaaa52d5f617569968b66eeb"
-});
--- evented_todos/README.md
+++
@@ -1,3 +0,0 @@
-#Simple Todos App
-Login and create todos
-Demo here: http://colby-todos.meteor.com
