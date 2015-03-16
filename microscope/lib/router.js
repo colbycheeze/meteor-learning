8672942889ae20f2dc4e3ee390ab66e8d4949753
Colby Williams <colby@colbycheeze.com>
2015-03-16 00:15:12 -0500

    Single page posts sub/pub added

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -14,7 +14,14 @@ Router.route('/posts/:_id/edit', {
 
 Router.route('/posts/:_id', {
   name: 'postPage',
-  waitOn: function() { return Meteor.subscribe('comments', this.params._id); },
+
+  waitOn: function() {
+    return [
+      Meteor.subscribe('singlePost', this.params._id),
+      Meteor.subscribe('comments', this.params._id)
+    ];
+  },
+
   data: function() { return Posts.findOne( this.params._id ); }
 });
 
